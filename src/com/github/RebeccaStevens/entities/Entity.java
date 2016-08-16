2ac80b0cb2f2c8c3126b46473af5d0575d795c92
slaymaster3000 <slaymaster3000@gmail.com>
2016-08-16 20:55:01 +1200

    added a few useful methods to the entity class

--- src/com/github/RebeccaStevens/entities/Entity.java
+++ src/com/github/RebeccaStevens/entities/Entity.java
@@ -21,6 +21,7 @@ public abstract class Entity {
 	 * @param y - The starting y position of the entity
 	 * @param width - The width of the entity
 	 * @param height - The height of the entity
+	 * @param color - The color of the entity
 	 */
 	public Entity(float x, float y, float width, float height, int color) {
 		this.position = new PVector(x, y);
@@ -62,6 +63,61 @@ public abstract class Entity {
 		float time = Time.getTimeStep();
 		this.position.x += time * this.velocity.x;
 		this.position.y += time * this.velocity.y;
+	}
+	
+	/**
+	 * The minimum x position of this entity i.e the left edge.
+	 * 
+	 * @return
+	 */
+	public float getMinX() {
+		return this.position.x - this.width / 2;
+	}
+	
+	/**
+	 * The maximum x position of this entity i.e the right edge.
+	 * 
+	 * @return
+	 */
+	public float getMaxX() {
+		return this.position.x + this.width / 2;
+	}
+	
+	/**
+	 * The minimum y position of this entity i.e the top edge.
+	 * 
+	 * @return
+	 */
+	public float getMinY() {
+		return this.position.y - this.height / 2;
+	}
+	
+	/**
+	 * The maximum y position of this entity i.e the bottom edge.
+	 * 
+	 * @return
+	 */
+	public float getMaxY() {
+		return this.position.y + this.height / 2;
+	}
+	
+	/**
+	 * Test if this entity is colliding with another entity.
+	 * 
+	 * @param other - The other entity to test with
+	 * @return True if this entity is touching the other entity
+	 */
+	public boolean collidesWith(Entity other) {
+		if (this == other) {
+			return true;
+		}
+		if (this.getMinX() <= other.getMaxX()
+		&&  this.getMaxX() >= other.getMinX()
+		&&  this.getMinY() <= other.getMaxY()
+		&&  this.getMaxY() >= other.getMinY()) {
+			return true;
+		}
+		return false;
 	}
 
 }
